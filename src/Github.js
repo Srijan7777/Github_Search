8b3f769c2e0ff4fb5258bb5abd6df800b8bb8e5b
srijan <srijanbharadwaj7777@gmail.com>
2023-04-17 05:46:23 +0530

    addded files
    added

---
+++ src/Github.js
@@ -0,0 +1,14 @@
+import React , {Component} from "react";
+
+class Github extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h2>I am coming from Github</h2>
+            </div>
+        );
+    }
+}
+export default Github;
